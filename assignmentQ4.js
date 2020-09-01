1644090a25510c5c8a88e835c810e19cabe33edc
Ravi Jaiswal <47974567+rjhacker360@users.noreply.github.com>
2020-09-01 14:47:24 +0530

    Add files via upload

---
+++ assignmentQ4.js
@@ -0,0 +1,20 @@
+//Question4: Program to display only elements containing 'a' in them from a array
+
+let Myarr = ["ravi", "Abhishek", "orange", "grapes"];
+var r=Myarr.includes("a");
+for( i=0; i<Myarr.length; i++){
+    {
+        console.log(Myarr[i]);
+    }
+}
+
+
+if (r > 0) {
+    console.log(Myarr[i]);
+}
+
+
+
+
+
+
