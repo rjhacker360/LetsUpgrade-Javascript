554b9a3924d631a4751967fb8a681a4ed8c23a60
Ravi Jaiswal <47974567+rjhacker360@users.noreply.github.com>
2020-09-01 12:29:37 +0530

    Add files via upload

---
+++ assignmentQ3.js
@@ -0,0 +1,11 @@
+//Question3: Program to search for a element in a array of strings
+
+
+let arr = Array("apple","mango","banana","grapes");
+        for(let i=0 ; i<arr.length ; i++)
+        {
+            if(arr[i].includes("e"))
+            {
+                console.log(arr[i]);
+            }
+        }
